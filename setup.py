72659d25f9533f9697c1ae90d6ade8fc55681ece
chappertron <52535659+chappertron@users.noreply.github.com>
2024-08-16 13:53:47 +0100

    Create release 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="multipole_dens",
-    version="0.1.0",
+    version="0.2.0",
     author="Aidan C",
     packages=["multipole"],
     scripts=["bin/calc_multipoles_dens"],
